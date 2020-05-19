befc8ad7dc5b7216dfb7f6b837d70dc928921426
Hat1ko <pasha1apple@gmail.com>
2020-05-19 10:25:00 +0300

    lab3 task2

--- lab3/js/task2.js
+++ lab3/js/task2.js
@@ -8,18 +8,28 @@ function tabFunc() {
     let tb = document.createElement("TABLE");
     tb.setAttribute("id", "myTable");
     document.body.appendChild(tb);
-    let sum = 0;
-    for (let a = 2; a < 4; a += (4 - 2) / parseFloat(document.getElementById("N-value").value)) {
+    var xmin = 1000, ymin = 1000, xmax = -1000, ymax= -1000
+    //b - a = 2 - (-1) = 3
+    for (let a = -1; a <= 2; a += 3 / parseFloat(document.getElementById("N-value").value)) {
         let tr = document.createElement("TR"),
-            y = ((Math.sin(a) + Math.pow(a, 4)) / Math.log(a));
-        sum += y;
+            y = 1 - a - 2 / Math.pow((a + 2), 2);
+        
         let td1 = document.createElement("TD");
         td1.appendChild(document.createTextNode("x=" + a.toPrecision(4)));
         let td2 = document.createElement("TD");
         td2.appendChild(document.createTextNode("y=" + y.toPrecision(6)));
+        if (a > xmax) xmax = a
+        if (y > ymax) ymax = y
+        if (a < xmin) xmin = a
+        if (y < ymin) ymin = y
         tr.appendChild(td1);
         tr.appendChild(td2);
         document.getElementById("myTable").appendChild(tr);
     }
-    alert("Сума усіх y=" + sum);
+    if (xmin * ymax > xmax * ymin) {
+        alert("xmin * ymax > xmax * ymin | : " + xmin * ymax)
+    } else {
+        alert("xmin * ymax < xmax * ymin | : " + xax * ymin)
+    }
+    
 }
